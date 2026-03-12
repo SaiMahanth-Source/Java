998b508d1c4d313a5af29a8814b69fa6fee14680
SaiMahanth-Source <saimahanthnagendla3@gmail.com>
2026-03-13 01:07:10 +0530

    Adding FuncInterface to staging

---
+++ src/FuncInterface.java
@@ -0,0 +1,16 @@
+@FunctionalInterface
+interface func{
+    void show(); // Functional interace will be having only one abstract method
+}
+
+public class FuncInterface {
+    public static void main(String[] args) {
+
+        func obj = new func(){
+            public void show(){
+                System.out.println("Hello World");
+            }
+        };
+        obj.show();
+    }
+}
