382e242754d460c1fc166acd7947dacac3c621d9
SaiMahanth-Source <saimahanthnagendla3@gmail.com>
2026-04-01 23:45:20 +0530

    Crucial topic in java

---
+++ src/MultipleThreads.java
@@ -0,0 +1,42 @@
+class AThread extends Thread{
+    public void run(){ // run method executes because of the concept of method overriding
+
+        for(int i = 0; i< 119 ; i++){
+            System.out.println("In class ATread");
+//            try{
+//                Thread.sleep(11);
+//            }
+//            catch(InterruptedException e){
+//                System.out.println(e);
+//            }
+//             this is to make it delay for some time so that other thread can proceed with execution
+        }
+    }
+}
+
+class BThread extends Thread{
+    public void run(){ // run method executes because of the concept of method overriding
+
+        for(int i = 0; i< 119 ; i++){
+            System.out.println("In class BTread");
+//            try{
+//                Thread.sleep(11);
+//            }
+//            catch(InterruptedException e){
+//                System.out.println(e);
+//            }
+//            this is to make it delay for some time so that other thread can proceed with execution
+        }
+    }
+}
+
+public class MultipleThreads {
+    public static void main(String[] args){
+
+        AThread athread = new AThread();
+        BThread bthread = new BThread();
+
+        athread.start(); // start method is to start the thread which belong to Thread class
+        bthread.start();
+    }
+}
